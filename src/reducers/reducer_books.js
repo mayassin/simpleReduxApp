c4f8d98110baa27d1c883b1c80bac6d04c15dd8e
Mahmoud Yassin <mahmoudayassin@gmail.com>
2018-10-09 01:11:04 +0200

    first and last commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function(){
+    return [
+        {title: 'JavaScript: The good parts', pages: 101},
+        {title: 'Harry Potter', pages: 50},
+        {title: 'The Dark Tower', pages: 30},
+        {title: 'Eloquent Ruby', pages: 211},
+        {title: 'The Catcher in the rye', pages: 500}
+
+    ];
+}
